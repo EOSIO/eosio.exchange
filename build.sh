f704d5df9863f2ff01ca2bc9b8b3637c85424081
Bucky Kittinger <larrykittinger@gmail.com>
2018-06-22 15:35:39 -0400

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,17 +5,19 @@ unamestr=`uname`
 
 if [[ "${unamestr}" == 'Darwin' ]]; then
    PREFIX=/usr/local
+   BOOST=/usr/local/include
 else
-   PREFIX=/opt
+   PREFIX=~/opt
+   BOOST=~/opt/boost_1_67_0/include
 fi
 
 mkdir -p bin/${CONTRACT_NAME}
 ### BUILD THE CONTRACT
-EOSCLANG="${PREFIX}/wasm/bin/clang++ -I/usr/local/include/libc++/upstream/include -I/usr/local/include/musl/upstream/include -I/usr/local/include "
+EOSCLANG="${PREFIX}/wasm/bin/clang++ -Iinclude -I/usr/local/include/libc++/upstream/include -I/usr/local/include/musl/upstream/include -I/usr/local/include -I${BOOST} "
 LINK="${PREFIX}/wasm/bin/llvm-link -only-needed "
 LLC="${PREFIX}/wasm/bin/llc -thread-model=single --asm-verbose=false"
-S2W="${PREFIX}/bin/eosio-s2wasm "
-W2W="${PREFIX}/bin/eosio-wast2wasm "
+S2W="/usr/local/bin/eosio-s2wasm "
+W2W="/usr/local/bin/eosio-wast2wasm "
 ${EOSCLANG}  -Iinclude -c -emit-llvm -O3 --std=c++14 --target=wasm32 -nostdinc -DBOOST_DISABLE_ASSERTS -DBOOST_EXCEPTION_DISABLE -nostdlib -nostdlibinc -ffreestanding -nostdlib -fno-threadsafe-statics -fno-rtti -fno-exceptions -o ${CONTRACT_NAME}.bc src/${CONTRACT_NAME}.cpp
 ${LINK} -o linked.bc ${CONTRACT_NAME}.bc /usr/local/usr/share/eosio/contractsdk/lib/eosiolib.bc /usr/local/usr/share/eosio/contractsdk/lib/libc++.bc /usr/local/usr/share/eosio/contractsdk/lib/libc.bc
 ${LLC} -o ${CONTRACT_NAME}.s linked.bc
@@ -24,6 +26,7 @@ ${W2W} ${CONTRACT_NAME}.wast bin/${CONTRACT_NAME}/${CONTRACT_NAME}.wasm -n
 cp abi/${CONTRACT_NAME}.abi bin/${CONTRACT_NAME}/${CONTRACT_NAME}.abi
 
 if [[ "$1" == 'noinstall' ]]; then
+   rm ${CONTRACT_NAME}.bc linked.bc ${CONTRACT_NAME}.wast ${CONTRACT_NAME}.s
    exit 0
 fi
 
